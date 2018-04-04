13b47f89ca8a846097dcde63e415d6acd47c772e
sunzhaoyuan <sunzhao.cool@gmail.com>
2018-04-04 00:07:49 -0400

    Add SceneLoader GameObject
    The SceneLoader GO can load a scene with a progress bar showing

---
+++ Assets/_Scripts/Supports/LevelLoader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LevelLoader : MonoBehaviour
+{
+
+	[Header ("Call LoadLevel when loading a scene.")]
+	public GameObject loadingScreen;
+	public Slider slider;
+
+
+	/// <summary>
+	/// Call this method to load a scene by its name.
+	/// </summary>
+	///
+	/// <param name="sceneToLoad">
+	/// 	Name of the Scene to load.
+	/// </param>
+	public void LoadLevel (string sceneToLoad)
+	{
+		StartCoroutine (LoadAsynchronously (sceneToLoad));
+	}
+
+
+	/// <summary>
+	/// 	Loads the scene and reports the Progress constently untile it's done.
+	/// </summary>
+	///
+	/// <param name="sceneToLoad">
+	///		Name of the Scene to load.
+	/// </param>
+	private IEnumerator LoadAsynchronously (string sceneToLoad)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync (sceneToLoad);
+		loadingScreen.SetActive (true);
+
+		while (!operation.isDone) {
+			// Clamp progress from .9 to 1.
+			float progress = Mathf.Clamp01 (operation.progress / .9f);
+			// update progress with slider
+			slider.value = progress;
+
+			yield return null;
+		}
+	}
+}
