26971769250bf2906564f191c242eb3b29f664d4
sunzhaoyuan <sunzhao.cool@gmail.com>
2018-04-05 23:04:11 -0400

    Add support for topdown camera
    See demo; still not natural

---
+++ Assets/_Scripts/Supports/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+	public Transform target;
+	[Space]
+	public float smoothness = 10f;
+	[Range (3f, 15f)]
+	public float x;
+	[Range (3f, 15f)]
+	public float y;
+	[Range (3f, 15f)]
+	public float z;
+	//	public Vector3 offset;
+
+	void LateUpdate ()
+	{
+		Vector3 offset = new Vector3 (x, y, z);
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothness * Time.deltaTime);
+		transform.position = smoothedPosition;
+
+		transform.LookAt (target);
+	}
+}
