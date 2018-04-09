2b56cacb84c27186350268c2f58ab59daebef89f
sunzhaoyuan <sunzhao.cool@gmail.com>
2018-04-09 19:39:37 -0400

    Add AEnemy class and Aniki class
    Weimu Song, Zhaoyuan Sun, Yuqi Zhou

---
+++ Assets/_Scripts/Enemies/AEnemy.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Abstract enemy class. All enemies classes should extend it.
+/// </summary>
+public class AEnemy : MonoBehaviour
+{
+
+	private bool _IsWeak = false;
+
+	public enum State
+	{
+		IDLE,
+		MOVE,
+		ATTACK,
+		DIED
+	}
+
+
+	public string Name {
+		get;
+	}
+
+	public float MaxHP {
+		get;
+	}
+
+	public float HP {
+		get;
+		set;
+	}
+
+	public float NextAttackTime {
+		get;
+		set;
+	}
+
+	public Dictionary<string, ASkill> Skills {
+		get;
+		set;
+	}
+
+	public ASkill CurrentSkill {
+		get;
+		set;
+	}
+
+	public float Speed {
+		get;
+		set;
+	}
+
+	public Vector3 Position {
+		get { return transform.position; }
+		set { transform.position = value; }
+	}
+
+	public float WeakDuraction {
+		get;
+		set;
+	}
+
+	public float WeakStartTime {
+		get;
+		set;
+	}
+
+	public State CurrentState {
+		get;
+		set;
+	}
+
+	public bool IsWeak {
+		get;
+		set;
+	}
+
+	/// <summary>
+	/// This method deal with damage taken from the bullet
+	/// </summary>
+	/// <param name="int damage">
+	/// HP -= damage;
+	/// if (IsWeak) { HP -= 0.3*MaxHP; CurrentState = State.IDLE; }
+	/// </param>.
+	public virtual void DamageTaken ()
+	{
+		
+	}
+}
---
+++ Assets/_Scripts/Enemies/Aniki.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Aniki - The first boss.
+/// </summary>
+public class Aniki : AEnemy
+{
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+}
