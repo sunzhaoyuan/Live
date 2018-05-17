adfec8d336f643053e6eb25471b7a79fdf3a4805
Yuqi Zhou <zhouy9@rose-hulman.edu>
2018-05-16 20:38:40 -0400

    add enemy's stuffs, abort merge

---
+++ Assets/_Scripts/PlayerAlt/Gun/GrenadeLightBlink.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeLightBlink : MonoBehaviour
+{
+
+	public float BlinkPeriod;
+	private Light light;
+
+	// Use this for initialization
+	void Start ()
+	{
+		light = gameObject.GetComponent<Light> ();
+		StartCoroutine (WaitForNextBlink ());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+
+	}
+
+	IEnumerator WaitForNextBlink ()
+	{
+		while (true) {
+			Debug.Log ("Boo");
+			light.enabled = !light.enabled;
+			yield return new WaitForSeconds (BlinkPeriod);
+		}
+
+	}
+}
---
+++ Assets/_Scripts/UI/RecoverPlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoverPlayerHealth : MonoBehaviour
+{
+
+	public bool IsPlayerInTrigger;
+	public float RecoveryRate;
+	private Player player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find ("Player").GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+		if (IsPlayerInTrigger && player.CurrentHP < player.MaxHP) {
+			Debug.Log ("Recovering");
+			player.CurrentHP += RecoveryRate;
+		}
+	}
+
+	/// <summary>
+	/// If player enter this area (trigger), recover its health
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag.Equals ("Player")) {
+			IsPlayerInTrigger = true;
+		}
+	}
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.tag.Equals ("Player")) {
+			IsPlayerInTrigger = false;
+		}
+	}
+
+}
